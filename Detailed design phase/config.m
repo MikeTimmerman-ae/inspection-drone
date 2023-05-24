eb95699494dd5d9e0b757ba7acbedf6002eff98d
Radu Serşeniuc <r.c.serseniuc-1@student.tudelft.nl>
2023-05-24 12:10:05 +0200

    cox

--- Detailed design phase/config.m
+++ Detailed design phase/config.m
@@ -14,15 +14,15 @@ Iz = 0.135532;      % kgm^2
 param.I = diag([Ix Iy Iz]);
 
 param.g = 9.81;           % m/s^2
-lx = 0.4389087297;  % m
-ly = 0.4389087297;  % m
+d_cgtop = 0.4389087297;  % m
 
 kF = -4.66925492e-4;            % N/(rad/s)^2
 kM = 1.45073741e-5;             % Nm/(rad/s)^2
+k_loss = 1.219;
 
 G = [kF kF kF kF;
-    0 kF*ly 0 -kF*ly;
-    -kF*lx 0 kF*lx 0;
+    0 kF*d_cgtop 0 -kF*d_cgtop;
+    -kF*d_cgtop 0 kF*d_cgtop 0;
     kM -kM kM -kM];
 
 Ginv = inv(G);
