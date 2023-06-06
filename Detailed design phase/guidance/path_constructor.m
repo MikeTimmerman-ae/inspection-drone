44aef3a9ca16206d9e74dfd16feae58ca9267176
Jip van Sommeren <jip.van.sommeren@gmail.com>
2023-06-06 15:39:54 +0200

    ddfj

--- Detailed design phase/guidance/path_constructor.m
+++ Detailed design phase/guidance/path_constructor.m
@@ -12,7 +12,7 @@ rng(100,"twister");
 From = [0 0 0];
 To = [500 500 0];
 
-targets = [[300, 240, 0];[700, 240,  0];[700, 730,  0];[300, 730,  0];];
+targets = [[0, 0, 0];[300, 230, 0];[700, 230,  0];[700, 730,  0];[300, 730,  0];];
 disp(size(targets, 1));
 %%%%%%%%%%%
 test_map = load("testing_map2.mat");
@@ -31,22 +31,30 @@ omap.FreeThreshold = omap.OccupiedThreshold;
 % %display(From)
 % To = [To(1:3) (eul2quat(To(4:6)))];
 
+% 
+% [waypoints, nWayPoints, states] = wp(From, To, omap);
+% [waypoints1, nWayPoints1] = wp(To, From, omap);
+waypoints_tot = [];
+states_tot = [];
 
-[waypoints, nWayPoints, states] = wp(From, To, omap);
-[waypoints1, nWayPoints1] = wp(To, From, omap);
-for i = 1:sizeof(targets,1)
-    [waypoints, nWayPoints, states] = wp(From, To, omap);
+for i = 2:size(targets,1)
+    % disp(targets(i-1, :))
+    % disp(i)
+    [waypoints, nWayPoints, states] = wp(targets(i-1, :), targets(i, :), omap);
+    % disp(waypoints)
+    
+    waypoints_tot = cat(1, waypoints_tot, waypoints(2:size(waypoints, 1), :));
+    states_tot = cat(1, states_tot, states(2:size(states, 1), :));
+end
+    % waypoints_tot = [waypoints_tot, waypoints];
+disp(waypoints_tot);
+disp(states_tot);
 
-    waypoints = cat(1, waypoints, waypoints1(2:size(waypoints1, 1), :));
-
-disp(waypoints);
-disp(states);
 
 
-waypoints_tot = cat(1, waypoints, waypoints1(2:size(waypoints1, 1), :));
 
 
-number_of_points = nWayPoints;
+number_of_points = size(states_tot, 1);
 
 mode_vector = zeros(number_of_points, 1)+2;
 mode_vector(1, 1) =  1;
@@ -62,13 +70,13 @@ waypoint_struct(1).mode = uint8(1) ;
 waypoint_struct(1).position = single([0;0;-15]);
 waypoint_struct(1).params = single([0;0;0;0]);
 waypoint_struct(number_of_points).mode = uint8(7) ;
-waypoint_struct(number_of_points).position = single([waypoints(number_of_points,1);waypoints(number_of_points,2);-15]);
+waypoint_struct(number_of_points).position = single([states_tot(number_of_points,1);states_tot(number_of_points,2);-15]);
 waypoint_struct(number_of_points).params = single([-1;-1;-1;-1]);
 
 
 for i = 2:number_of_points-1
     waypoint_struct(i).mode = uint8(mode_vector(i,1)) ;
-    waypoint_struct(i).position = single([waypoints(i,1);waypoints(i,2);-15]);
+    waypoint_struct(i).position = single([states_tot(i,1);states_tot(i,2);-15]);
     waypoint_struct(i).params = single([0;0;0;0]);
 end
 
@@ -80,16 +88,15 @@ waypoint_struct= outWPS;
 
 show(omap)
 hold on
-scatter(From(1),From(2),30,".r")
-scatter(To(1),To(2),30,".g")
+scatter(targets(1,1),targets(1,2),30,".r")
+scatter(targets(size(targets, 1),1),targets(size(targets, 1),2),30,".g")
 
 % Plot the waypoints
 
-plot(waypoints(:,1),waypoints(:,2), "-g")
-
-
+plot(waypoints_tot(:,1),waypoints_tot(:,2), "-g")
+plot(states_tot(:,1),states_tot(:,2), "-y")
 
 view([-31 63])
-legend("Start Position","Goal Position","Planned Path")
+legend("Start Position","Goal Position","Planned Path", "Optimize Path")
 hold off
 
